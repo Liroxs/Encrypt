8668a76df112915a825a099da08e380ccef44b7e
Lirox's Web <101893028+Liroxs@users.noreply.github.com>
2022-10-27 21:16:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import base64
 from cgitb import text
+from logging import root
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
@@ -23,7 +24,6 @@ class App:
         frame.grid(row=0, column=0, sticky=N+S+E+W)
         
         Grid.columnconfigure(frame, 1, weight=1) 
-        
         Grid.rowconfigure(frame, 3, weight=1)
         
         self.create_menu(master)
@@ -47,14 +47,17 @@ class App:
         helpmenu = Menu(menu, tearoff=False)
         menu.add_cascade(label="Help", menu=helpmenu)
         helpmenu.add_command(label="About...", command=self.About)
+        helpmenu.add_command(label="Origine", command=self.Origine)
+        helpmenu.add_command(label="Discord", command=self.Discord)
         helpmenu.configure(bg='red')
+    
 
     def add_controlli(self, master, frame, parametre):
         mylist=['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512', "md5"]
         self.var = StringVar(master)
         self.var.set("md5")
 
-        ttk.Label(frame, text="String").grid(row=0, sticky=W, padx=5, pady=5)
+        ttk.Label(frame, text="Chaine de caractere").grid(row=0, sticky=W, padx=5, pady=5)
         ttk.Label(frame, text="Type de hash").grid(row=1, sticky=W, padx=5, pady=5)
         ttk.Button(frame, text="Encrypter", command=self.calcola_stringa).grid(row=1, sticky=W, padx=5, pady=5)
         ttk.Label(frame, text="Resultat encrypter").grid(row=2, sticky=W, padx=5, pady=5)
@@ -72,14 +75,15 @@ class App:
             self.e1.insert(0, parametre)
     
     def create_statusBar(self,frame):
-        self.status_bar = ttk.Label(frame,background="#80D0D0",relief=FLAT,text="> $ Yazuko and Lirox")
-        self.status_bar.grid(row=3, columnspan=2, sticky=S+W+E)
+        self.status_bar = ttk.Label(frame,background="#80D0D0",relief=FLAT,text="> $ Yazuko and Lirox's")
+        self.status_bar.grid(row=3, columnspan=3, sticky=S+W+E)
     
         
     def calcola_stringa(self):
         str = self.e1.get()
         if str == "":
             messagebox.showinfo("Attention", "Veuillez définir une chaine de caractère à encrypter")
+            messagebox.showinfo("Report", "En cas de probleme merci de contacter $$$#7296 et Yazuko")
             return
             
         m = self.get_type_of_hash()
@@ -105,7 +109,15 @@ class App:
     def About(self):
         messagebox.showinfo("Info", "Pour toute question(s) ou autre(s) merci de contacter sur Discord : $$$#7296 ou 0xYazuko#9521") 
         messagebox.showinfo("Project", "Project made by Lirox's and Yazuko 2022®")
+
+    def  Discord(self):
+        messagebox.showinfo("Discord", "$$$#7296 and 0xYazuko #9521")
     
+    def Origine(self): 
+        messagebox.showinfo("Origine", "L'origine de £ncrypt viens tout simplement d'une idee de Yazuko et Lirox's ce projet est à la base fait pour vous simplifiez la vie.(Encrypter)")
+
+     
+
 
 def main(argv):
     if len(sys.argv) > 1:
@@ -114,7 +126,7 @@ def main(argv):
         parametre = None
 
     root = Tk()
-    root.wm_title("£ncrypt®")
+    root.wm_title("£ncrypt 2022®")
     app = App(root,parametre)
     root.iconbitmap('icona.ico')
     root.wm_title = ttk.Label(root, background="red",relief=FLAT,)
